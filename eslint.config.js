fce6d69151c57bdeb2177f97da052dacce54906e
joe <risnerjoseph@gmail.com>
2023-09-19 23:17:09 -0600

    Update eslint rule for no-unused-vars

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,7 @@ export default [
     rules: {
       'max-nested-callbacks': ['error', 3],
       'no-return-await': 'error',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }]
     }
   }
