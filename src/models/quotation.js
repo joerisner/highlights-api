ba18024c013f1812db2b6eb7e2ade5f12e6a00f3
joe <risnerjoseph@gmail.com>
2023-09-29 11:19:40 -0600

    Add additional find methods to quotation model

--- src/models/quotation.js
+++ src/models/quotation.js
@@ -6,10 +6,34 @@ const { quotations } = JSON.parse(await readFile(jsonUrl, 'utf8'));
 
 export const findQuotationById = quotationId =>
   new Promise((resolve, reject) => {
-    const quotation = quotations.find(quotation => quotation.id === quotationId);
+    const result = quotations.find(quotation => quotation.id === quotationId);
 
-    if (quotation) resolve(quotation);
+    if (result) resolve(result);
     reject(new Error(`Could not find a quotation with id ${quotationId}`));
+  });
+
+export const findQuotationsByAuthorId = authorId =>
+  new Promise((resolve, reject) => {
+    const results = quotations.filter(quotation => quotation.authorId === authorId);
+
+    if (results.length) resolve(results);
+    reject(new Error(`Could not find any quotations for Author ID ${authorId}`));
+  });
+
+export const findQuotationsBySourceId = sourceId =>
+  new Promise((resolve, reject) => {
+    const results = quotations.filter(quotation => quotation.sourceId === sourceId);
+
+    if (results.length) resolve(results);
+    reject(new Error(`Could not find any quotations for Source ID ${sourceId}`));
+  });
+
+export const findQuotationsByTagId = tagId =>
+  new Promise((resolve, reject) => {
+    const results = quotations.filter(quotation => quotation.tagIds.includes(tagId));
+
+    if (results.length) resolve(results);
+    reject(new Error(`Could not find any quotations for Tag ID ${tagId}`));
   });
 
 export const numQuotations = () => quotations.length;
